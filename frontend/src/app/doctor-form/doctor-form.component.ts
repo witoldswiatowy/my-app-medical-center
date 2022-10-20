ce8daaacfa992b1ddc9510a3689c5a5992dc27ed
Witold Światowy <witold.swiatowy@capgemini.com>
2022-10-20 22:17:15 +0200

    doctor-form - frontend - post methode

--- frontend/src/app/doctor-form/doctor-form.component.ts
+++ frontend/src/app/doctor-form/doctor-form.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {AddDoctorRequest, MedicalSpecialization} from "../model/doctor";
+import {AddDoctorRequest, Doctor, MedicalSpecialization} from "../model/doctor";
 import {HttpClient} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-doctor-form',
@@ -20,13 +22,36 @@ export class DoctorFormComponent implements OnInit {
   }
 
   constructor(
-    private httpClient: HttpClient
-  ) { }
+    private httpClient: HttpClient,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
   sendAddDoctorRequest(): void {
+    this.httpClient.post<Doctor>("http://localhost:8080/api/doctor", this.request)
+      .subscribe({
+        next: (data) => {
+          this.snackBar.open('Add doctor successfully!', undefined, {
+            verticalPosition: 'top',
+            horizontalPosition: 'start',
+            duration: 2000
+          })
+
+          this.router.navigate(['/doctors'])
+        },
+        error: (error) => {
+          console.log('Error : ' + error)
+          this.snackBar.open('Failed to add doctor!', undefined, {
+            verticalPosition: 'top',
+            horizontalPosition: 'start',
+            duration: 2000
+          })
+        }
+      })
   }
 
 }
