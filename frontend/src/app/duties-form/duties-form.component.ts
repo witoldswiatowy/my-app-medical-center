609075cf3a48541baa5aae93779852f2a71817ce
Witold Światowy <witold.swiatowy@capgemini.com>
2022-10-26 16:12:29 +0200

    add_duty - frontend - initial commit with component and button in duties-list

---
+++ frontend/src/app/duties-form/duties-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-duties-form',
+  templateUrl: './duties-form.component.html',
+  styleUrls: ['./duties-form.component.css']
+})
+export class DutiesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
